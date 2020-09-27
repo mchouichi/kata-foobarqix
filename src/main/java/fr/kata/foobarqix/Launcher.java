c35949bbcc0a4306aa264317448da2ce35a0fe96
Asus <Asus@DESKTOP-M8H6D16.home>
2020-09-27 15:00:40 +0200

    unit test 1 (division by 3 ) : test failed

---
+++ src/main/java/fr/kata/foobarqix/Launcher.java
@@ -0,0 +1,22 @@
+package fr.kata.foobarqix;
+
+/**
+ * 
+ * @author mchouichi Main Class
+ *
+ */
+
+public class Launcher {
+
+	public final static String FOO = "Foo";
+
+	public static void main(String[] args) {
+
+	}
+
+	public static String applyFooBarQixRules(int numb) {
+
+		return null;
+	}
+
+}
---
+++ src/test/java/fr/kata/test/ut/FooBarQixTest.java
@@ -0,0 +1,21 @@
+package fr.kata.test.ut;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import fr.kata.foobarqix.Launcher;
+
+/**
+ * 
+ * @author mchouichi Unit Test Class
+ *
+ */
+
+public class FooBarQixTest {
+
+	@Test
+	public void dividedOnlyBy3Test() {
+		Assert.assertTrue(Launcher.applyFooBarQixRules(9).equals(Launcher.FOO));
+	}
+
+}
