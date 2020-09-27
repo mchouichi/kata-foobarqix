e8206f7d1bc4847126159cf9c78cebf79631224e
Asus <Asus@DESKTOP-M8H6D16.home>
2020-09-27 15:39:13 +0200

    UT6 (contains many 5) : test failed

--- src/test/java/fr/kata/test/ut/FooBarQixTest.java
+++ src/test/java/fr/kata/test/ut/FooBarQixTest.java
@@ -17,25 +17,30 @@ public class FooBarQixTest {
 	public void dividedOnlyBy3Test() {
 		Assert.assertTrue(Launcher.applyFooBarQixRules(9).equals(Launcher.FOO));
 	}
-	
+
 	@Test
 	public void containsOnlyOne3Test() {
 		Assert.assertTrue(Launcher.applyFooBarQixRules(13).equals(Launcher.FOO));
 	}
-	
+
 	@Test
 	public void containsOnlyMany3Test() {
 		Assert.assertTrue(Launcher.applyFooBarQixRules(33).equals("FooFooFoo"));
 	}
-	
+
 	@Test
 	public void dividedOnlyBy5Test() {
 		Assert.assertTrue(Launcher.applyFooBarQixRules(10).equals(Launcher.BAR));
 	}
-	
+
 	@Test
 	public void containsOnlyOne5Test() {
 		Assert.assertTrue(Launcher.applyFooBarQixRules(59).equals(Launcher.BAR));
+	}
+
+	@Test
+	public void containsOnlyMany5Test() {
+		Assert.assertTrue(Launcher.applyFooBarQixRules(55).equals("BarBarBar"));
 	}
 
 }
